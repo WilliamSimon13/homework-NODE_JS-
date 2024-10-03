35fcfe29c5aafcaa9d9d0f81f5c1cbdf6cccee00
WilliamSimon13 <maithieutin@gmail.com>
2024-10-03 20:24:50 +0700

    Homework Case#6

--- Homework4/js/product.js
+++ Homework4/js/product.js
@@ -43,16 +43,23 @@ Save();
        for (var i in product){
            var data = JSON.parse(JSON.stringify(product[i]))
         var listproduct = '<div class="col-lg-3 col-md-6 col-sm-6 col-6 mt-3">';
-        listproduct += '<div class="card product p-2" styte="width:auto">'; 
+        listproduct += '<div class="card product p-2" style="width:auto">'; 
         listproduct += '<img class="card-img-top" src="img/' + data.img +'" alt="...">';
         listproduct += '<div class="card-title product-title text-center h5" >'+data.name+'</div>';
         listproduct += '<div class="price text-center h6">'+data.price+'₫</div>';
-        listproduct +=  '<span class="text-center add-to-cart btn btn-outline-warning add-cart" data-id="'+data.id+'" data-name="'+data.name+'" data-img="'+data.img+'" data-price="'+data.price+'" onclick="tks()">';
-        // listproduct +=  '<a class=" add-to-cart btn" data-name="'+product[i].name+'" data-price="'+product[i].price+'" onclick="tks()">';
-        listproduct +=  '<a>';
-        listproduct +=  '<i class="fas fa-cart-plus"></i>';
-        listproduct +=  '</a>';
-        listproduct +=  '</span>';
+        listproduct += `<button style="
+                                background-color: #28a745; 
+                                color: white; 
+                                border: none; 
+                                padding: 10px 20px; 
+                                font-size: 16px; 
+                                border-radius: 5px; 
+                                cursor: pointer;
+                                display: flex;
+                                align-items: center;
+                                justify-content: center;
+                            ">Xem Chi Tiết</button>
+                            `;
         listproduct += '</div>';
         listproduct += '</div>';
 
