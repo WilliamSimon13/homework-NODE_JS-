6fe28de4b94cac2b24d8775165b73269691529ab
WilliamSimon13 <maithieutin@gmail.com>
2024-10-10 18:16:16 +0700

    HomeWork6 #case7

---
+++ nodeJS_VNUK/HW6.js
@@ -0,0 +1,40 @@
+// function tinhdiem(diem) {
+//     if (diem < 4) {
+//         console.log("Hoc Lai");
+//     } else if (diem >= 4 && diem < 5.5) {
+//         console.log("Loai D");
+//     } else if (diem >= 5.5 && diem < 7) {
+//         console.log("Loai C");
+//     } else if (diem >= 7 && diem < 8.5) {
+//         console.log("Loai B");
+//     } else if (diem >= 8.5 && diem <= 10) {
+//         console.log("Loai A");
+//     } else {
+//         console.log("Nhap lai diem");
+//     }
+// }
+
+// var a = 12;
+// var b = 20;
+// if(a<b){
+//     let tmp =a;
+//     a = b;
+//     b = tmp;
+// }
+// console.log(a,b);
+
+// var hello = (name, message) =>{
+//     console.log("chao"+ name + ", loi nhan:" +message);
+// };
+// hello ("simon", "???");
+
+var dientichHCN = (width, height) =>{
+    return width * height;
+}
+
+var chuviHCN = (width, height) =>{
+    return (width + height) *2;
+}
+
+console.log(dientichHCN(10,20));
+console.log(chuviHCN(10,20));
