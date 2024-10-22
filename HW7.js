323e9bd8e56ddbad8d6d256d5451c97d7fec43cc
WilliamSimon13 <maithieutin@gmail.com>
2024-10-22 18:39:55 +0700

    HW7 CAse#2

---
+++ HW7.js
@@ -0,0 +1,62 @@
+
+let mySet = new Set();
+
+
+mySet.add(1);
+mySet.add(5);
+mySet.add(1);  
+mySet.add('Hello');
+
+
+console.log(mySet);  
+
+
+console.log(mySet.size); 
+
+
+console.log(mySet.has(1));  
+console.log(mySet.has(10)); 
+
+
+mySet.delete(5);
+console.log(mySet); 
+
+
+mySet.forEach(value => {
+  console.log(value);
+});
+
+
+mySet.clear();
+console.log(mySet); 
+
+
+
+let myMap = new Map();
+
+myMap.set('name', 'John');
+myMap.set('age', 30);
+myMap.set('occupation', 'Developer');
+
+
+console.log(myMap.get('name'));
+console.log(myMap.get('age'));   
+
+
+console.log(myMap.size);
+
+
+console.log(myMap.has('occupation'));  
+console.log(myMap.has('salary'));      
+
+
+myMap.delete('age');
+console.log(myMap.size);  
+
+
+myMap.forEach((value, key) => {
+  console.log(`${key}: ${value}`);
+});
+
+myMap.clear();
+console.log(myMap.size);  
